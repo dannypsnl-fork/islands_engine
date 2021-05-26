d4bbfd0d1cd1ad35fc0906890c28325c70de84c9
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-27 02:52:30 +0800

    add_coordinates

--- lib/islands_engine/islands_engine/islands_engine/island.ex
+++ lib/islands_engine/islands_engine/islands_engine/island.ex
@@ -1,10 +1,26 @@
 defmodule IslandsEngine.Island do
-  alias IslandsEngine.{Coordinate, Guesses}
+  alias IslandsEngine.{Coordinate, Island}
 
   @enforce_keys [:coordinates, :hit_coordinates]
   defstruct [:coordinates, :hit_coordinates]
 
   def new() do
     %Island{coordinates: MapSet.new(), hit_coordinates: MapSet.new()}
+  end
+
+  def add_coordinates(offsets, upper_left) do
+    Enum.reduce_while(offsets, MapSet.new(), fn offset, acc ->
+      add_coordinate(acc, upper_left, offset)
+    end)
+  end
+
+  def add_coordinate(coordinates, %Coordinate{row: row, col: col}, {row_offset, col_offset}) do
+    case Coordinate.new(row + row_offset, col + col_offset) do
+      {:ok, coordinate} ->
+        {:cont, MapSet.put(coordinates, coordinate)}
+
+      {:error, :invalid_coordinate} ->
+        {:halt, {:error, :invalid_coordinate}}
+    end
   end
 end
