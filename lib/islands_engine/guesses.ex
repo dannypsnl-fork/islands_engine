47bebcd2d04729ec8b382222413f8f9220ede031
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-05-27 15:59:07 +0800

    update guess

--- lib/islands_engine/guesses.ex
+++ lib/islands_engine/guesses.ex
@@ -9,4 +9,12 @@ defmodule IslandsEngine.Guesses do
   def new() do
     %Guesses{hits: MapSet.new(), misses: MapSet.new()}
   end
+
+  def add(%Guess{} = guess, :hit, %Coordinate{} = coordinate) do
+    update_in(guess.hits, &MapSet.put(&1, coordinate))
+  end
+
+  def add(%Guess{} = guess, :miss, %Coordinate{} = coordinate) do
+    update_in(guess.misses, &MapSet.put(&1, coordinate))
+  end
 end
